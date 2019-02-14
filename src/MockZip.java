95c6eb25f3e9953c116a36eaa4efb6128192c99d
John Mercer <36443454+jm421@users.noreply.github.com>
2019-02-14 11:22:26 +0000

    Add files via upload
    An example class to show how multiple files can be compressed into a zip file in java

---
+++ src/MockZip.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
+public class MockZip {
+    public static void main(String[] args) throws IOException {
+    List<String> srcFiles =
+        Arrays.asList(
+            "C:\\Users\\John\\Desktop\\GuitarZero\\src\\test1.txt",
+            "C:\\Users\\John\\Desktop\\GuitarZero\\src\\test2.txt");          // files to be zipped go here
+
+        FileOutputStream fos = new FileOutputStream("multiCompressed.zip");   // title goes here
+
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        for (String srcFile : srcFiles) {
+            File fileToZip = new File(srcFile);
+            FileInputStream fis = new FileInputStream(fileToZip);
+            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+            zipOut.putNextEntry(zipEntry);
+
+            byte[] bytes = new byte[1024];
+            int length;
+            while((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+            fis.close();
+        }
+        zipOut.close();
+        fos.close();
+    }
+}
