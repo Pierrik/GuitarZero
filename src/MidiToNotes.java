d3096cb2be581cde7abda5f4b522b927f7fdb415
tommansfield5 <tommansfield51@gmail.com>
2019-02-15 14:19:56 +0000

    1

--- src/MidiToNotes.java
+++ src/MidiToNotes.java
@@ -9,11 +9,6 @@ import java.util.Map;
 import java.util.HashMap;
 /**
  * Convert MIDI file to note file.
- *
- * Version 1 Harper
- * Version 2 Kamilla
- * Version 3 Tom
- * Version 4 John
  */
 public class MidiToNotes {
     String file;
@@ -22,11 +17,12 @@ public class MidiToNotes {
     }
 
 
-    //commit12
-
     public static void formatNote(long tick, int n, Map<Long, String> m) {
         final int note = n % 6;
         String format = "";
+
+        // Each note in the song is formatted to a 3 digit number
+        // Representing 3 lanes of guitar highway
         switch ( note ) {
             case 0: format = "100"; break;
             case 1: format = "200"; break;
