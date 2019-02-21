49667e68f1ad775c7386b9e9827b250ca05973df
tommansfield5 <tommansfield51@gmail.com>
2019-02-21 12:16:26 +0000

    New midi to notes file

--- tests/MidiToNotesTest.java
+++ tests/MidiToNotesTest.java
@@ -7,13 +7,19 @@ import javax.sound.midi.MidiSystem;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * MidiToNotes Test
+ * Junit 4.12
+ * @author Tom Mansfield
+ */
+
 public class MidiToNotesTest {
 
   @Test
   public void testFormatNote0() {
     // Note 0 should be converted to "100";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,0,m);
+    MidiToNotesOld.formatNote(1,0,m);
     Long key = new Long(1);
     assertEquals("100", m.get(key));
 
@@ -25,7 +31,7 @@ public class MidiToNotesTest {
   public void testFormatNote1() {
     // Note 1 should be converted to "200";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,1,m);
+    MidiToNotesOld.formatNote(1,1,m);
     Long key = new Long(1);
     assertEquals("200", m.get(key));
 
@@ -37,7 +43,7 @@ public class MidiToNotesTest {
   public void testFormatNote2() {
     // Note 1 should be converted to "010";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,2,m);
+    MidiToNotesOld.formatNote(1,2,m);
     Long key = new Long(1);
     assertEquals("010", m.get(key));
 
@@ -49,7 +55,7 @@ public class MidiToNotesTest {
   public void testFormatNote3() {
     // Note 1 should be converted to "020";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,3,m);
+    MidiToNotesOld.formatNote(1,3,m);
     Long key = new Long(1);
     assertEquals("020", m.get(key));
 
@@ -61,7 +67,7 @@ public class MidiToNotesTest {
   public void testFormatNote4() {
     // Note 1 should be converted to "001";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,4,m);
+    MidiToNotesOld.formatNote(1,4,m);
     Long key = new Long(1);
     assertEquals("001", m.get(key));
 
@@ -73,7 +79,7 @@ public class MidiToNotesTest {
   public void testFormatNote5() {
     // Note 1 should be converted to "002";
     Map<Long, String> m = new HashMap<>();
-    MidiToNotes.formatNote(1,5,m);
+    MidiToNotesOld.formatNote(1,5,m);
     Long key = new Long(1);
 
     assertEquals("002", m.get(key));
@@ -87,10 +93,10 @@ public class MidiToNotesTest {
     Map<Long, String> m = new HashMap<>();
 
     // Should be formatted as "100"
-    MidiToNotes.formatNote(1,0,m);
+    MidiToNotesOld.formatNote(1,0,m);
 
     // Should be formatted as "200"
-    MidiToNotes.formatNote(1,1,m);
+    MidiToNotesOld.formatNote(1,1,m);
 
     Long key = new Long(1);
 
@@ -105,10 +111,10 @@ public class MidiToNotesTest {
   public void testFormatNoteCombineDifferentLane(){
     Map<Long, String> m = new HashMap<>();
     // Should be formatted as "100"
-    MidiToNotes.formatNote(1,0,m);
+    MidiToNotesOld.formatNote(1,0,m);
 
     // Should be formatted as "010"
-    MidiToNotes.formatNote(1,2,m);
+    MidiToNotesOld.formatNote(1,2,m);
 
     Long key = new Long(1);
 
@@ -122,20 +128,20 @@ public class MidiToNotesTest {
   @Test
   public void testCompare1() {
     // Should return the note "212";
-    assertEquals("212", MidiToNotes.compare("210","012") );
+    assertEquals("212", MidiToNotesOld.compare("210","012") );
   }
 
   @Test
   public void testCompare2() {
     // Should return "000"
-    assertEquals("000", MidiToNotes.compare("000","000") );
+    assertEquals("000", MidiToNotesOld.compare("000","000") );
   }
 
   @Test
   public void testCompareInvalid1() {
     // Should give exception
     try {
-      MidiToNotes.compare("abc", "def");
+      MidiToNotesOld.compare("abc", "def");
     } catch (Exception e) {
       assertTrue(true);
     }
@@ -145,7 +151,7 @@ public class MidiToNotesTest {
   public void testCompareInvalid2() {
     // Should give exception
     try {
-      MidiToNotes.compare("", "");
+      MidiToNotesOld.compare("", "");
     } catch (Exception e) {
       assertTrue(true);
     }
@@ -155,7 +161,7 @@ public class MidiToNotesTest {
   public void testGetTracks() {
     // Exception should not be thrown
     try {
-      MidiToNotes.findTracks(
+      MidiToNotesOld.findTracks(
           MidiSystem.getSequence(
               new File("C:\\Users\\tomma\\Desktop\\GuitarZero\\AC_DC_-_Back_In_Black.mid")
           )
@@ -169,7 +175,7 @@ public class MidiToNotesTest {
   @Test
   public void testGetTracksInvalid(){
     try {
-      MidiToNotes.findTracks(MidiSystem.getSequence(new File("invalid file")));
+      MidiToNotesOld.findTracks(MidiSystem.getSequence(new File("invalid file")));
       fail();
     } catch (InvalidMidiDataException e) {
       e.printStackTrace();
@@ -184,7 +190,7 @@ public class MidiToNotesTest {
   public void testWriteTrackInvalid() {
     // Should throw exception
     try {
-      MidiToNotes.writeTrack(MidiSystem.getSequence(new File("invalid file")).getTracks()[0]);
+      MidiToNotesOld.writeTrack(MidiSystem.getSequence(new File("invalid file")).getTracks()[0]);
       fail();
     } catch (InvalidMidiDataException e) {
       assertTrue(true);
@@ -197,7 +203,7 @@ public class MidiToNotesTest {
   public void testWriteTrack(){
     // Should run successfully
     try {
-      MidiToNotes.writeTrack(MidiSystem.getSequence(new File("C:\\Users\\tomma\\Desktop\\GuitarZero\\AC_DC_-_Back_In_Black.mid")).getTracks()[0]);
+      MidiToNotesOld.writeTrack(MidiSystem.getSequence(new File("C:\\Users\\tomma\\Desktop\\GuitarZero\\AC_DC_-_Back_In_Black.mid")).getTracks()[0]);
     } catch (InvalidMidiDataException e) {
       fail();
     } catch (IOException e) {
@@ -209,7 +215,7 @@ public class MidiToNotesTest {
   public void testWriteFileInvalid(){
     // Exception should be handled
     try {
-      MidiToNotes.writeFile("invalid file");
+      MidiToNotesOld.writeFile("invalid file");
     } catch (Exception e) {
       fail();
     }
