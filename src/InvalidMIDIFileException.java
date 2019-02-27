8eacf245e5ff388b0e1721d1470321eab6035f69
tommansfield5 <tommansfield51@gmail.com>
2019-02-27 20:40:56 +0000

    New midi to notes file

--- src/InvalidMIDIFileException.java
+++ src/InvalidMIDIFileException.java
@@ -1,6 +1,5 @@
 /**
  * Can be thrown when a selected MIDI file is not suitable for conversion for the game
- * @author Tom Mansfield
  */
 public class InvalidMIDIFileException extends Exception {
   public InvalidMIDIFileException(String message){
