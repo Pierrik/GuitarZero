85a3cc8d65c7a2b54e92dde3102ffeff7de8979b
HarperFord <44643411+HarperFord@users.noreply.github.com>
2019-02-28 12:43:33 +0000

    Add files via upload

---
+++ src/PlayMode.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+/**
+ * Play Mode.
+ *
+ * @author Harper Ford
+ * @version 1.00, February 2019.
+*/
+public class PlayMode extends JPanel{
+    /**
+     * Initialises the GUI classes for a PlayMode
+     */
+    public PlayMode() {
+        // Initialise the model, controller, view GUI classes
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        PlayModeView view = new PlayModeView();
+        this.add(view);
+        //PlayModeModel      model      = new PlayModeModel(view);
+        //PlayModeController controller = new PlayModeController(model);
+        //controller.pollGuitarForever();
+    }
+}
