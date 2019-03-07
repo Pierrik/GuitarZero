ec50238477b200ea589fa2c9fef70a77e272d64c
Kamila <kh503@exeter.ac.uk>
2019-03-07 14:59:45 +0000

    extends jpanel

--- src/StoreMode.java
+++ src/StoreMode.java
@@ -2,13 +2,20 @@ import java.io.File;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * StoreMode.
  * @author John Mercer
- * @version 1.0, March 2019
+ * @author Kamila Hoffmann-derlacka
+ * @version 1.1, March 2019
  */
-public class StoreMode {
+public class StoreMode extends JPanel {
+
+  CarouselView view;
+  StoreModeModel model;
+  StoreModeController controller;
+
   // server settings
   private static final String HOST = "localhost";
   private static final int    PORT = 8888;
@@ -17,7 +24,7 @@ public class StoreMode {
   private static final String BUNDLES = System.getProperty("user.dir") + "/local_store/bundle_files/";
   private static final String PREVIEWS = System.getProperty("user.dir") + "/local_store/preview_files/";
 
-  public static void main(String args[]) {
+  public StoreMode() {
     MockClient client = new MockClient(HOST, PORT);
 
     // Getting all available songs on the server
@@ -42,8 +49,10 @@ public class StoreMode {
     StoreModeModel      model      = new StoreModeModel();
     StoreModeController controller = new StoreModeController(model);
     CarouselView        view       = new CarouselView(menuOptions);
-    view.setVisible(true);
-    controller.pollGuitarForever();
+    Thread controllerThread = new Thread(controller);
+    controllerThread.start();
+    this.add(view);
+    //view.setVisible(true);
 
   }
 }
