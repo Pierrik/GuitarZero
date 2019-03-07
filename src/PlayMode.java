36875fd5d0aa0c1f509d51a7a2a16e43dfb462b5
tommansfield5 <tommansfield51@gmail.com>
2019-03-07 15:39:42 +0000

    New midi to notes file

--- src/PlayMode.java
+++ src/PlayMode.java
@@ -16,13 +16,16 @@ import java.lang.Thread;
  * @version 2.00, March 2019.
 */
 public class PlayMode extends JPanel implements Runnable{
-    /**
-     * Initialises the GUI classes for a PlayMode
-     */
+
     PlayModeView view;
     PlayModeModel model;
     PlayModeController controller;
-    public PlayMode(String bundlePath) {
+
+  /**
+   * Sets up the Play Mode, Initialises Model, View and Controller
+   * @param bundlePath the path of the bundle directory to play the song
+   */
+  public PlayMode(String bundlePath) {
 
         // Initialise the model, controller, view GUI classes
         view = new PlayModeView();
@@ -31,13 +34,21 @@ public class PlayMode extends JPanel implements Runnable{
         controller = new PlayModeController(model);
         this.add(view);
         view.setVisible(true);
-        //controller.pollGuitarForever();
+
     }
 
-    public void run() {
+  /**
+   * The thread for Play Mode
+   */
+  public void run() {
       Thread modelThread = new Thread(model);
       Thread controllerThread = new Thread(controller);
+
+      // Start the game
       modelThread.start();
+
+      // Start the controller thread to run alongside the game
+      // Limit the speed of the frames
       controllerThread.start();
       long targetTime = 30;
       while(true){
@@ -46,16 +57,20 @@ public class PlayMode extends JPanel implements Runnable{
         long elapsed = System.nanoTime() - s;
 
         long wait = targetTime - elapsed / 1000000;
-        //System.out.println(wait);
+
         try {
           Thread.sleep(wait);
         } catch (Exception o) {
           o.printStackTrace();
         }
 
+        // If a note has passed the screen without being played, drop the note
         if(view.dropNote) {
           model.dropNote();
-          //System.out.println("Note Dropped");
+
+          // Testing
+          System.out.println("Note Dropped");
+
           view.dropNote = false;
         }
       }
